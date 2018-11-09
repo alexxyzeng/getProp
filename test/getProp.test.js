8360c17b744ea2a4fefb2fb4b25bda04717bedf9
xiayao <xiayao.zeng@gmail.com>
2018-11-09 08:25:33 +0800

    Update test/getProp.test.js

--- test/getProp.test.js
+++ test/getProp.test.js
@@ -20,19 +20,19 @@ describe('test get property from non-object or null', () => {
   it('get property from null with default value', () => {
     expect(getProp(null, 'meetings.list', [])).toEqual([]);
   });
-})
+});
 
 describe('test get property', () => {
-  let testObj = {a: 1, b: {}, c: [] };
-  beforeEach(() => {
-    testObj = {a: 1, b: { d: { q: 3 }}, c: [] };
-  })
+  let testObj = {a: 1, b: { d: { q: 3 }}, c: [] };
 
-  it('get property existed correctly', () => {
+  it('get property existed', () => {
     expect(getProp(testObj, 'a')).toEqual(1);
     expect(getProp(testObj, 'c')).toEqual([]);
-    expect(getProp(testObj, 'b.d.q')).toEqual(3);
+  });
+
+  it('get nested property', () => {
     expect(getProp(getProp(testObj, 'b.d'))).toEqual({ q: 3 });
+    expect(getProp(testObj, 'b.d.q')).toEqual(3);
   })
 
   it('get undefined property', () => {
@@ -45,15 +45,15 @@ describe('test get property', () => {
     expect(getProp(testObj, 'b.c', 1)).toEqual(1);
   });
 
-  it('return undefined from the first value in empty array without crash', () => {
+  it('return undefined from the element in empty array', () => {
     expect(getProp(testObj.c, 0)).toBeUndefined()
   });
 
   it('return default value from the element in empty array', () => {
     expect(getProp(testObj.c, 2, '2')).toEqual('2');
-  })
+  });
 
-  it('get elment out of the range of array without crash', () => {
+  it('get elment out of the range of array', () => {
     expect(getProp([1, 2, 3], 10)).toBeUndefined();
-  })
+  });
 });
